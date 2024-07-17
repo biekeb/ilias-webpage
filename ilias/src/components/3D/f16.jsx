e3d53c48a3f31af4f4c798baa37fcc007c1e98e0
hadrienallemon <hadrienallemon@gmail.com>
2024-07-17 12:28:14 +0200

    geometry

--- ilias/src/components/3D/f16.jsx
+++ ilias/src/components/3D/f16.jsx
@@ -9,61 +9,77 @@ export default function F16() {
   const scroll = useScroll();
   const tl = useRef();
   const [hovered, setHovered] = useState(false);
+  const getOffset = () => {
+    if (scroll.offset < .25)
+      return tl.current
+        .to(plane.current.position, { x: 0, y: -4, duration: 70 }, 1)
+        .to(plane.current.position, { z: -5, duration: 40, delay: .8 }, 1)
+        .to(plane.current.rotation, { y: Math.PI / -1.7, duration: 40, delay: .8 }, 1)
+    if (scroll.offset < .4)
+      return tl.current
+        .to(plane.current.position, { x: -5, y: -4, duration: 120 }, 1)
+        .to(plane.current.rotation, { y: Math.PI / -2.7, duration: 40, delay: .8 }, 1)
+    if (scroll.offset < .5)
+      return tl.current
+        .to(plane.current.position, { x: 5, y: -4, duration: 200 }, 1)
+        .to(plane.current.rotation, { y: Math.PI / -1.5, duration: 40, delay: .8 }, 1)
+    else
+      return 4
+  };
 
   useFrame((state, delta) => {
-    tl.current.seek(scroll.offset * tl.current.duration());
+    getOffset();
   });
 
   useLayoutEffect(() => {
     tl.current = gsap.timeline({
-      defaults: { duration: 2, ease: "power1.inOut" },
+      // defaults: { duration: 0, ease: "power1.inOut" },
     });
 
-    tl.current
-      .to(plane.current.position, { y: 0 }, 0)
-      .to(plane.current.position, { x: 0 }, 0)
-      .to(plane.current.position, { z: -14 }, 0)
+    // tl.current
+    //   // .to(plane.current.position, { y: -20, x: -5, z: -2 }, 0)
+    //   .to(plane.current.position, { x: 0, y: -4, z: -5 }, 1)
 
-      .to(plane.current.rotation, { y: Math.PI / -2 }, 1)
-      .to(plane.current.position, { z: 0 }, 1)
+    //   .to(plane.current.rotation, { y: Math.PI / -2 }, 1)
+    // .to(plane.current.position, { z: 0 }, 2)
 
-      .to(plane.current.position, { x: 0 }, 1)
-      .to(plane.current.position, { y: 0 }, 3)
+    //     .to(plane.current.position, { x: 0 }, 1)
+    //     .to(plane.current.position, { y: 0 }, 3)
 
-      .to(plane.current.position, { x: -2 }, 6)
+    //     .to(plane.current.position, { x: -2 }, 6)
 
-      .to(plane.current.position, { x: 2 }, 9)
+    //     .to(plane.current.position, { x: 2 }, 9)
 
-      .to(plane.current.position, { x: -10 }, 14)
+    //     .to(plane.current.position, { x: -10 }, 14)
 
-      .to(plane.current.position, { x: -15 }, 20);
+    //     .to(plane.current.position, { x: -15 }, 20);
   }, []);
 
-  useEffect(() => {
-    if (hovered) {
-      gsap.to(plane.current.position, {
-        y: 0.3,
-        x: 0.3,
-        duration: 0.8,
-        ease: "power3.out",
-      });
-    } else {
-      gsap.to(plane.current.position, {
-        y: 0,
-        x: 0,
-        duration: 0.8,
-        ease: "power3.out",
-      });
-    }
-  }, [hovered]);
+  // useEffect(() => {
+  //   if (hovered) {
+  //     gsap.to(plane.current.position, {
+  //       y: 0.3,
+  //       x: 0.3,
+  //       duration: 0.8,
+  //       ease: "power3.out",
+  //     });
+  //   } else {
+  //     gsap.to(plane.current.position, {
+  //       y: 0,
+  //       x: 0,
+  //       duration: 0.8,
+  //       ease: "power3.out",
+  //     });
+  //   }
+  // }, [hovered]);
 
   return (
     <primitive
       dispose={null}
       ref={plane}
       object={f16.scene}
-      rotation={[0, Math.pi, 0]}
-      position={[-15, -5, -2]}
+      rotation={[0, 0, 0]}
+      position={[-30, -5, -15]}
       scale={[0.05, 0.05, 0.05]}
       onPointerOver={() => setHovered(true)}
       onPointerOut={() => setHovered(false)}
--- ilias/src/pages/PlaneScreen.jsx
+++ ilias/src/pages/PlaneScreen.jsx
@@ -1,8 +1,9 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect, useMemo } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import {
   Float,
   Scroll,
+  useScroll,
   ScrollControls,
   Clouds,
   Cloud,
@@ -17,8 +18,30 @@ import { BackgroundCanvas } from "../components/background/BackgroundCanvas";
 import AxesHelper from "./AxesHelper";
 import { useGLTF } from "@react-three/drei";
 
-const spaceshipURL =
-  "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/low-poly-spaceship/model.gltf";
+
+const CloudsWithScroll = () => {
+  const ref = useRef();
+  const scroll = useScroll();
+
+  useFrame(() => {
+    if (!scroll) return;
+    const opacity = 1 - Math.min(scroll.offset * 3, 1); // Adjust the multiplier and range as needed
+    if (ref.current) {
+      ref.current.children.forEach((cloud) => {
+        if (!cloud.material) return;
+        cloud.material.opacity = opacity;
+      });
+    }
+  });
+
+  return (
+    <Clouds ref={ref} material={THREE.MeshLambertMaterial} limit={400}>
+      <Cloud position={[-4, -2, 0]} speed={0.2} opacity={0.2} />
+      <Cloud position={[4, -2, 0]} speed={0.2} opacity={0.5} />
+      <Cloud position={[0, 0, 0]} speed={0.2} opacity={0.2} />
+    </Clouds>
+  );
+};
 
 export const PlaneScreen = () => {
   const [section, setSection] = useState(0);
@@ -41,29 +64,29 @@ export const PlaneScreen = () => {
 
   const [offset, setOffset] = useState(0);
 
-  console.log(offset);
+  // console.log(offset);
 
-  useEffect(() => {
-    setInterval(() => setOffset(scroll.current), 100);
-  }, []);
+  // useEffect(() => {
+  //   setInterval(() => setOffset(scroll.current), 100);
+  // }, []);
 
-  useEffect(() => {
-    const scrollCount = (e) => {
-      scroll.current = e.target.scrollTop;
-    };
-    setTimeout(
-      () =>
-        document
-          .querySelectorAll("canvas[data-engine*='three.js']+div")[0]
-          .addEventListener("scroll", scrollCount),
-      2000
-    );
-    return () => {
-      document
-        .querySelectorAll("canvas[data-engine*='three.js']+div")?.[0]
-        ?.removeEventListener("scroll", scrollCount);
-    };
-  }, []);
+  // useEffect(() => {
+  //   const scrollCount = (e) => {
+  //     scroll.current = e.target.scrollTop;
+  //   };
+  //   setTimeout(
+  //     () =>
+  //       document
+  //         .querySelectorAll("canvas[data-engine*='three.js']+div")[0]
+  //         .addEventListener("scroll", scrollCount),
+  //     2000
+  //   );
+  //   return () => {
+  //     document
+  //       .querySelectorAll("canvas[data-engine*='three.js']+div")?.[0]
+  //       ?.removeEventListener("scroll", scrollCount);
+  //   };
+  // }, []);
 
   // This is where you place the updated useEffect block
   useEffect(() => {
@@ -76,13 +99,13 @@ export const PlaneScreen = () => {
     }
   }, [offset]);
 
-  const handleButtonClick = () => {
-    setIsModalOpen(true);
-  };
+  // const handleButtonClick = () => {
+  //   setIsModalOpen(true);
+  // };
 
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-  };
+  // const handleCloseModal = () => {
+  //   setIsModalOpen(false);
+  // };
 
   return (
     <>
@@ -108,16 +131,11 @@ export const PlaneScreen = () => {
           <ambientLight intensity={3} />
 
           {/* Particles */}
-          <MorphingParticles blendFactor={blendFactor} offset={offset} />
+          <MorphingParticles />
 
           <ScrollControls pages={7} damping={0.1}>
-            <Scroll>
-              <Clouds material={THREE.MeshLambertMaterial} limit={400}>
-                <Cloud position={[-4, -2, 0]} speed={0.2} opacity={0.2} />
-                <Cloud position={[4, -2, 0]} speed={0.2} opacity={0.5} />
-                <Cloud position={[0, 0, 0]} speed={0.2} opacity={0.2} />
-              </Clouds>
-            </Scroll>
+            {/* <CloudsWithScroll /> */}
+
             <Float
               speed={0.5}
               rotationIntensity={0.5}
@@ -132,12 +150,10 @@ export const PlaneScreen = () => {
             </Scroll>
           </ScrollControls>
 
-          {/* <Particles parameters={parameters} offset={offset} /> */}
-
           <AxesHelper size={5} />
         </Canvas>
       </MotionConfig>
-      {offset >= 1500 && offset < 1900 && (
+      {/* {offset >= 1500 && offset < 1900 && (
         <Button onClick={handleButtonClick} />
       )}
       {isModalOpen && <Modal onClose={handleCloseModal} />}
@@ -155,16 +171,16 @@ export const PlaneScreen = () => {
       {offset >= 3500 && offset < 4500 && (
         <Button4 onClick={handleButtonClick} />
       )}
-      {isModalOpen && <Modal onClose={handleCloseModal} />}
+      {isModalOpen && <Modal onClose={handleCloseModal} />} */}
     </>
   );
 };
-
-const MorphingParticles = ({ blendFactor, offset }) => {
+const MorphingParticles = () => {
   const particlesRef = useRef();
-  const torusKnotGeometry1 = new THREE.SphereGeometry(1, 32, 32);
-  const torusKnotGeometry2 = new THREE.SphereGeometry(1, 32, 32);
-  const sphereGeometry = new THREE.TorusKnotGeometry(1, 0.3, 100, 16);
+  const torusKnotGeometry1 = useMemo(() => new THREE.SphereGeometry(1, 32, 32), []);
+  const torusKnotGeometry2 = useMemo(() => new THREE.SphereGeometry(1, 32, 32), []);
+  const sphereGeometry = useMemo(() => new THREE.TorusKnotGeometry(1, 0.3, 100, 16), []);
+  const [rotation, setRotation] = useState(0);
 
   const particlesCount = Math.min(
     torusKnotGeometry1.attributes.position.count,
@@ -174,6 +190,7 @@ const MorphingParticles = ({ blendFactor, offset }) => {
 
   const initialRandomPositions = useRef(new Float32Array(particlesCount * 3));
   const positions = new Float32Array(particlesCount * 3);
+  const positionsFunnel = new Float32Array(particlesCount * 3);
 
   // Generate initial random positions for the particles
   useEffect(() => {
@@ -184,159 +201,120 @@ const MorphingParticles = ({ blendFactor, offset }) => {
     }
   }, [particlesCount]);
 
-  // Interpolate between random positions, TorusKnot, and Sphere positions
-  for (let i = 0; i < particlesCount; i++) {
-    const randomPosition = new THREE.Vector3(
-      initialRandomPositions.current[i * 3],
-      initialRandomPositions.current[i * 3 + 1],
-      initialRandomPositions.current[i * 3 + 2]
-    );
+  // Generate funnel positions
+  const generateFunnelPositions = () => {
+    const positions = new Float32Array(particlesCount * 3);
+    for (let i = 0; i < particlesCount; i++) {
+      const theta = (i / particlesCount) * Math.PI * 2;
+      const radius = (particlesCount - i) / particlesCount * 2;
+      const x = torusKnotGeometry2.attributes.position.getX(i)
+      const y =  torusKnotGeometry1.attributes.position.getY(Math.round(particlesCount/2.3)); // Funnel height
+      const z = radius * Math.sin(theta);
 
-    const torusPosition1 = new THREE.Vector3(
-      torusKnotGeometry1.attributes.position.getX(i) - 2,
-      torusKnotGeometry1.attributes.position.getY(i),
-      torusKnotGeometry1.attributes.position.getZ(i)
-    );
-
-    const torusPosition2 = new THREE.Vector3(
-      torusKnotGeometry2.attributes.position.getX(i) + 2,
-      torusKnotGeometry2.attributes.position.getY(i),
-      torusKnotGeometry2.attributes.position.getZ(i)
-    );
-
-    const spherePosition = new THREE.Vector3(
-      sphereGeometry.attributes.position.getX(i) + 3,
-      sphereGeometry.attributes.position.getY(i),
-      sphereGeometry.attributes.position.getZ(i)
-    );
-
-    let blendedPosition = new THREE.Vector3();
-    if (offset < 2100) {
-      // From offset 0 to 2100, use random positions
-      blendedPosition = randomPosition;
-    } else if (offset < 3300) {
-      // From offset 2100 to 3300, transition from random positions to torus knots
-      // Calculate the blend factor 't' based on the current offset
-      const t = (offset - 2100) / 1200;
-      // Interpolate between random positions and torus knot positions
-      blendedPosition = new THREE.Vector3().lerpVectors(
-        randomPosition,
-        i % 2 === 0 ? torusPosition1 : torusPosition2,
-        t
-      );
-    } else if (offset < 4800) {
-      // From offset 3300 to 4800, stay at the torus knot positions
-      blendedPosition = i % 2 === 0 ? torusPosition1 : torusPosition2;
-    } else if (offset < 5000) {
-      // From offset 4800 to 5000, transition from torus knots to sphere
-      // Calculate the blend factor 't' based on the current offset
-      const t = (offset - 4800) / 200;
-      // Interpolate between torus knot positions and sphere position
-      const intermediateTorusPosition =
-        i % 2 === 0 ? torusPosition1 : torusPosition2;
-      blendedPosition = new THREE.Vector3().lerpVectors(
-        intermediateTorusPosition,
-        spherePosition,
-        t
-      );
-    } else {
-      // After offset 5000, use sphere positions
-      blendedPosition = spherePosition;
+      positions[i * 3] = x;
+      positions[i * 3 + 1] = y;
+      positions[i * 3 + 2] = z;
     }
+    return positions;
+  };
 
-    positions[i * 3] = blendedPosition.x;
-    positions[i * 3 + 1] = blendedPosition.y;
-    positions[i * 3 + 2] = blendedPosition.z;
-  }
+  // Interpolate between random positions, TorusKnot, and Funnel positions
+  const getSpheres = () => {
+    for (let i = 0; i < particlesCount; i++) {
+      const randomPosition = new THREE.Vector3(
+        initialRandomPositions.current[i * 3],
+        initialRandomPositions.current[i * 3 + 1],
+        initialRandomPositions.current[i * 3 + 2]
+      );
+
+      const torusPosition1 = new THREE.Vector3(
+        torusKnotGeometry1.attributes.position.getX(i) - 2,
+        torusKnotGeometry1.attributes.position.getY(i),
+        torusKnotGeometry1.attributes.position.getZ(i)
+      );
+
+      const torusPosition2 = new THREE.Vector3(
+        torusKnotGeometry2.attributes.position.getX(i) + 2,
+        torusKnotGeometry2.attributes.position.getY(i),
+        torusKnotGeometry2.attributes.position.getZ(i)
+      );
+
+      const blendedPosition = i % 2 === 0 ? torusPosition1 : torusPosition2;
+
+      positions[i * 3] = blendedPosition.x;
+      positions[i * 3 + 1] = blendedPosition.y;
+      positions[i * 3 + 2] = blendedPosition.z;
+    }
+  };
 
   const particlesGeometry = new THREE.BufferGeometry();
   particlesGeometry.setAttribute(
-    "position",
+    'position',
     new THREE.BufferAttribute(positions, 3)
+  );
+  const particlesGeometryFunnel = new THREE.BufferGeometry();
+  particlesGeometryFunnel.setAttribute(
+    'position',
+    new THREE.BufferAttribute(generateFunnelPositions(), 3)
   );
 
   const particlesMaterial = new THREE.PointsMaterial({
-    color: "#ffffff",
+    color: '#ffffff',
     sizeAttenuation: true,
     size: 0.03,
   });
 
+  // Create text sprites
+  const createTextSprite = (text, x, y) => {
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+    canvas.width = 100;
+    canvas.height = 100;
+
+    context.font = '50px Arial';
+    context.fillStyle = '#ffbbff';
+    context.fillText(text, 0, 50);
+
+    const texture = new THREE.CanvasTexture(canvas);
+    const spriteMaterial = new THREE.SpriteMaterial({ map: texture });
+    const sprite = new THREE.Sprite(spriteMaterial);
+    sprite.scale.set(1, 1, 1);
+    sprite.position.set(x, y, 0);
+
+    return sprite;
+  };
+
+  useEffect(() => {
+    if (particlesRef.current) {
+      const sprite1 = createTextSprite('ilias', -1.9, -.2);
+      const sprite2 = createTextSprite('Alis', 2.1, -.2);
+      particlesRef.current.add(sprite1);
+      particlesRef.current.add(sprite2);
+    }
+  }, [particlesCount]);
+
   useFrame(() => {
     if (particlesRef.current) {
-      particlesRef.current.position.x = offset >= 2000 ? 0 : -10; // Start the offset effect earlier
+      torusKnotGeometry1.rotateY(rotation);
+      torusKnotGeometry2.rotateY(rotation);
+      setRotation(rotation + 1 / 360);
+      getSpheres();
+      particlesRef.current.geometry.attributes.position.needsUpdate = true;
     }
   });
 
   return (
+    <>
     <points
       ref={particlesRef}
       geometry={particlesGeometry}
       material={particlesMaterial}
     />
-  );
-};
-
-const Button = ({ onClick }) => {
-  return (
-    <div className="gif-button">
-      <button onClick={onClick}>1</button>
-    </div>
-  );
-};
-
-const Modal = ({ onClose }) => {
-  return (
-    <div
-      style={{
-        position: "fixed",
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        backgroundColor: "rgba(0, 0, 0, 0.5)",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        zIndex: 200,
-      }}
-    >
-      <div
-        style={{
-          background: "white",
-          padding: "20px",
-          borderRadius: "10px",
-          maxWidth: "500px",
-          width: "100%",
-        }}
-      >
-        <h2>Modal Title</h2>
-        <p>This is a modal content.</p>
-        <button onClick={onClose}>Close</button>
-      </div>
-    </div>
-  );
-};
-
-const Button2 = ({ onClick }) => {
-  return (
-    <div className="gif-button2">
-      <button onClick={onClick}>2</button>
-    </div>
-  );
-};
-
-const Button3 = ({ onClick }) => {
-  return (
-    <div className="gif-button2">
-      <button onClick={onClick}>Ilias</button>
-    </div>
-  );
-};
-
-const Button4 = ({ onClick }) => {
-  return (
-    <div className="gif-button">
-      <button onClick={onClick}>alias</button>
-    </div>
+    <points
+      ref={particlesRef}
+      geometry={particlesGeometryFunnel}
+      material={particlesMaterial}
+    />
+    </>
   );
 };
